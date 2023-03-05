0fd9bd876f6924dfbdd9d5e8bce946e1958d1fa8
Nuraaab <ahmednuru215@gmail.com>
2023-03-05 21:21:57 +0300

    project modified

--- users/src/App.js
+++ users/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import UserList from './UserList';
 import  {BrowserRouter,Route,Routes} from 'react-router-dom';
 import CreateUser from './CreateUser';
-import UserDetail from './UserDetail';
 import EditUser from './EditUser';
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
       <Routes>
         <Route path='/' element={<UserList/>}> </Route>
         <Route path='/users/create' element={<CreateUser/>}> </Route>
-        <Route path='/users/detail/:userid' element={<UserDetail/>}> </Route>
         <Route path='/users/update/:userid' element={<EditUser/>}> </Route>
       </Routes>
       </BrowserRouter>
